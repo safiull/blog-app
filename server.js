57b8fb2cf26d1117287ffc4adb011758fe8c8278
safiull <safiullalam9931@gmail.com>
2025-07-25 11:27:50 +0600

    first commit

---
+++ server.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, 'public')));
