aef564e0b93cf0f80e3d806b61a9a800d97afe13
safiull <safiullalam9931@gmail.com>
2025-07-25 22:12:48 +0600

    working with template Mastering

--- server.js
+++ server.js
@@ -2,16 +2,18 @@
 // mkdir blog-app
 // cd blog-app
 // npm init -y
-// npm install express ejs
+// npm install express ejs express-ejs-layouts
 // npx nodemon server.js
 
 const express = require('express');
 const path = require('path');
 const app = express();
+const expressLayouts = require('express-ejs-layouts');
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(expressLayouts);
 
 const services = [
     {id: 1, title: "E-Commerce", icon: 'fas fa-shopping-cart fa-stack-1x fa-inverse', description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit.z"},
@@ -24,7 +26,7 @@ app.get('/', (req, res) => {
 });
 
 app.get('/login', (req, res) => {
-    res.render('auth/login');
+    res.render('auth/login', {layout: 'layouts/auth/index'});
 })
 
 app.listen(3000, () => {
