e58fd561fe444959a9c5e4b6ac2909ed177e9380
safiull <safiullalam9931@gmail.com>
2025-07-25 11:53:37 +0600

    service dynamic.

--- server.js
+++ server.js
@@ -6,8 +6,14 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
+const services = [
+    {id: 1, title: "E-Commerce", icon: 'fas fa-shopping-cart fa-stack-1x fa-inverse', description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit.z"},
+    {id: 1, title: "Responsive Design", icon: 'fas fa-laptop fa-stack-1x fa-inverse', description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit.z"},
+    {id: 1, title: "Web Security", icon: 'fas fa-lock fa-stack-1x fa-inverse', description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit.z"}
+]
+
 app.get('/', (req, res) => {
-    res.render('index');
+    res.render('index', {services});
 });
 
 app.listen(3000, () => {
