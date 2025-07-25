9d27308d23e2be8ab4e47ee6c2ec88a26307b13a
safiull <safiullalam9931@gmail.com>
2025-07-25 11:35:35 +0600

    blade is showing

--- server.js
+++ server.js
@@ -5,3 +5,11 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+app.listen(3000, () => {
+    console.log("Server is running 1.");
+});
